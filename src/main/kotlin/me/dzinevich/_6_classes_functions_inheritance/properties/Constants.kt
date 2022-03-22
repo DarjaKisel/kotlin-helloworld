c8e5034d2f82c2d4040ba39627e8c508bd2fbfe5
dzinevich <darjakisel@gmail.com>
2022-03-22 18:59:18 +0200

    Constants and data classes in Kotlin

---
+++ src/main/kotlin/me/dzinevich/_6_classes_functions_inheritance/properties/Constants.kt
@@ -0,0 +1,8 @@
+package me.dzinevich._6_classes_functions_inheritance.properties
+
+// top level constant
+val MY_CONSTANT = 100
+
+fun main() {
+    println(MY_CONSTANT)
+}
---
+++ src/main/kotlin/me/dzinevich/_6_classes_functions_inheritance/properties/DataClass.kt
@@ -0,0 +1,33 @@
+package me.dzinevich._6_classes_functions_inheritance.properties
+
+import me.dzinevich._3_basic_differences_kotlin_java.equality.Employee
+
+// main purpose - storing state
+// in data class we have: toString + equals + hashcode + copy
+// data class must have a primary constructor which must have at least 1 param, it must be val or var not just a parameter
+// data class can't be abstract or sealed or inner
+// you can declare other properties, other than in the prim constructor, but they're ignored by tostring/ equals/ hashcode
+data class Car(val color: String, val model: String, val year: Int) {
+}
+
+class RegularClass(var name: String = "abc")
+
+fun main() {
+    val car = Car("red", "opel corsa", 1991)
+    println(car.toString())
+
+    val car2 = Car("red", "opel corsa", 1991)
+
+    println(car == car2) // data class instances are equal by reference
+    println(car.equals(car2)) // data class instances are equal by structure
+
+    val reg = RegularClass()
+    val reg2 = RegularClass()
+    println(reg == reg2) // regular class instances are NOT equal by reference
+
+    //copy method
+    val car3 = car2.copy()
+    println(car3.toString())
+    val car4 = car.copy(year = 2015) // we can change some of the properties
+    println(car4.toString())
+}
