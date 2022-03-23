6c3d8296afc161bec3d1da4014673c7725fc4f44
dzinevich <darjakisel@gmail.com>
2022-03-23 17:54:14 +0200

    Inheritance in Kotlin

---
+++ src/main/kotlin/me/dzinevich/_6_classes_functions_inheritance/inheritance/Inheritance.kt
@@ -0,0 +1,67 @@
+package me.dzinevich._6_classes_functions_inheritance.inheritance
+
+// everything in kotlin is public and final by default
+// so you can't extend classes as they're final by default
+// you should say it is open explicitly
+
+open class Vehicle(val model: String) { }
+
+// subclass must call the parent constructor
+class Truck(model: String): Vehicle(model) {
+}
+
+// or
+//class Truck2: Vehicle {
+//    constructor(): super()
+//}
+
+// abstract
+
+abstract class AbsVehicle(val model: String) {
+    open fun printModel() = println("The model name: $model")
+    abstract fun thePrice(): Double
+}
+open class Car(model: String, horsePower: Int): AbsVehicle(model) {
+    final override fun printModel() = println("The CAR model name: $model")
+    override fun thePrice(): Double = 10099.99
+}
+
+// override also means open! you can also inherit it!
+
+class ElectricCar(model: String): Car(model, 150) {
+
+}
+
+fun main() {
+    val aCar = Car("Opel", 150)
+    aCar.printModel()
+
+    val smth = SomethingElse("abc")
+}
+
+// let's say we don't have a primary constructor
+open class Something {
+    val property: String
+    constructor(param: String) {
+        property = param
+        println("Hello from Something")
+    }
+}
+
+class SomethingElse: Something {
+    constructor(otherParam: String): super(otherParam) {
+        println("Hello from SomethingElse")
+    }
+}
+
+// can't extend data classes (open, abstract, etc..)
+//open data class Abcd(val prop: String) {} -> error
+
+
+
+
+
+
+
+
+
---
+++ src/main/kotlin/me/dzinevich/_6_classes_functions_inheritance/inheritance/Interfaces.kt
@@ -0,0 +1,30 @@
+package me.dzinevich._6_classes_functions_inheritance.inheritance
+
+interface MyInterface {
+    val num: Int
+    //val num2: Int = 2 - error, can't initialize
+    val num2: Int
+    get() { // have to use get() to have a concrete value
+        //return field*2 // can't use the backing filed 'field' in get() / set() in interfaces
+        return num*2 // can't use the backing filed 'field' in get() / set() in interfaces
+    }
+
+    fun myFun(str: String): String
+}
+
+// extendable by default, don't need 'open'
+interface MySubInterface: MyInterface {
+    fun mySubFun(num: Int): String
+}
+
+open class InterfaceImpl: MySubInterface {
+    override val num: Int = 24
+    override val num2: Int = 3
+
+    override fun mySubFun(num: Int): String {
+        TODO("Not yet implemented")
+    }
+    override fun myFun(str: String): String {
+        TODO("Not yet implemented")
+    }
+}
