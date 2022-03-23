1847e680a39d5f049ee8a7771bd39ccb73b40bb0
dzinevich <darjakisel@gmail.com>
2022-03-23 15:08:04 +0200

    Inline Functions in Kotlin

---
+++ src/main/kotlin/me/dzinevich/_6_classes_functions_inheritance/functions/InlineFunctions.kt
@@ -0,0 +1,9 @@
+package me.dzinevich._6_classes_functions_inheritance.functions
+
+fun function_NotInline(op1: Int, op2: Int, label: String) = "${label} ${op1 * op2}"
+
+// make it inline:
+// inline function is NOT compiled to a function at all
+// when it's compiled it's body is directly substituted to a function call
+
+inline fun function_Inline(op1: Int, op2: Int, label: String) = "${label} ${op1 * op2}"
