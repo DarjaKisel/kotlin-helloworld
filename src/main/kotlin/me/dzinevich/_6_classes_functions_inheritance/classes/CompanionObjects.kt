f0a3119e3f1eedc3d59e75e60a32b714c5ec8e85
dzinevich <darjakisel@gmail.com>
2022-03-23 18:42:55 +0200

    Companion Objects in Kotlin

---
+++ src/main/kotlin/me/dzinevich/_6_classes_functions_inheritance/classes/CompanionObjects.kt
@@ -0,0 +1,44 @@
+package me.dzinevich._6_classes_functions_inheritance.classes
+
+class SomeClass private constructor(val someString: String) {
+
+//    val privateVal = 6
+//    fun accessPrivateField() {
+//        println("The private field value is $privateVal")
+//    }
+
+    companion object SomeName { // similar to static in java, we access it without creating an instance
+        private val privateVal = 6
+        fun accessPrivateField() {
+            println("The private field value is $privateVal")
+        }
+
+        fun justAssign(str: String) = SomeClass(str)
+        fun lowerOrUpper(str: String, isLower: Boolean): SomeClass {
+            if (isLower) {
+                return SomeClass(str.lowercase())
+            } else {
+                return SomeClass(str.uppercase())
+            }
+        }
+    }
+}
+
+fun main() {
+    SomeClass.SomeName.accessPrivateField() // like static in java
+    // or
+    SomeClass.accessPrivateField()
+
+    // can't use private constructor
+    // let's create some instances using factory methods
+    val some1 = SomeClass.justAssign("abc")
+    val some2 = SomeClass.lowerOrUpper("abc", false)
+
+    println(some1.someString)
+    println(some2.someString)
+
+}
+
+// you can use companion objects to access private constructors
+// -> to implement a factory pattern!
+
