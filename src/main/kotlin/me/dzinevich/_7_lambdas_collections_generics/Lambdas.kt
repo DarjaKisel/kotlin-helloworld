188e1e2528e8e2f3fb42924d4c3eb4141f7e510f
dzinevich <darjakisel@gmail.com>
2022-03-25 15:33:27 +0200

    Lambdas in Kotlin

---
+++ src/main/kotlin/me/dzinevich/_7_lambdas_collections_generics/Lambdas.kt
@@ -0,0 +1,44 @@
+package me.dzinevich._7_lambdas_collections_generics
+
+fun main() {
+    // run { println("I'm in a lambda!") } // run a lambda using Run statement
+
+    val employees = listOf(
+        Employee("John", "Smith", 2015),
+        Employee("Josh", "Johnson", 2020),
+        Employee("Alex", "Long", 2018)
+    )
+    println(employees.minByOrNull { e -> e.startYear })
+    // we can use _it_ when there's a single parameter and compiler can infer the type
+    println(employees.minByOrNull { it.startYear })
+    // replace a lambda with a call to a _member reference_
+    println(employees.minByOrNull(Employee::startYear))
+    // or a call to a top level function
+    run(::topLevel)
+    run { topLevel() }
+
+    // you can access mutable vars from a lambda
+    // in java you can access only _final_ vals
+    var n = 10
+    run {
+        n +=10
+        println(n)
+    }
+}
+
+// can access function parameters but can't change them
+fun useParameter(emplList: List<Employee>, num: Int) {
+    emplList.forEach {
+        println(it.firstName)
+        println(num)
+    }
+}
+
+fun topLevel() {
+    println("I am in a top level function")
+}
+
+data class Employee(val firstName: String, val lastName: String, val startYear: Int) {
+
+}
+
