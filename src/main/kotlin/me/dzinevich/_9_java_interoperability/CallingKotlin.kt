d88c650976ef2a8f1837bf0e4fcf60ad265b5fde
dzinevich <darjakisel@gmail.com>
2022-04-01 11:39:04 +0300

    Annotations when calling Kotlin from Java.

--- src/main/kotlin/me/dzinevich/_9_java_interoperability/CallingKotlin.kt
+++ src/main/kotlin/me/dzinevich/_9_java_interoperability/CallingKotlin.kt
@@ -1,6 +1,9 @@
 @file:JvmName("StaticCar")
 package me.dzinevich._9_java_interoperability
 
+import java.io.IOException
+import kotlin.jvm.Throws
+
 fun topLevel() = println("I am in the cAr file!")
 
 class SuperCar(color: String, @JvmField val model: String, val year: Int, var isAuto: Boolean) {
@@ -9,6 +12,28 @@ class SuperCar(color: String, @JvmField val model: String, val year: Int, var is
         field = "always red"
     }
 
+    companion object {
+        @JvmField val isAuto = false
+        const val isExpensive = true
+        @JvmStatic fun carComp() = println("I am the computer!")
+    }
+
+    fun printMe(text: String) {
+        println("I dont really accept a null parameter! : $text")
+    }
+
+    @Throws(IOException::class)
+    fun doIO() {
+        throw IOException()
+    }
+
+    @JvmOverloads fun defaultArgs(string: String = "Default String") {
+        println("I am printing the value:::: $string")
+    }
+}
+
+object SingletonObj {
+    @JvmStatic fun doSomething() = println("I am a Singleton instance printing a line!")
 }
 
 fun main() {
